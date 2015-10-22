887ec2233434365776b6cde2a5564348fb0da296
Pedro Martins <pbmartins@ua.pt>
2015-10-22 13:45:45 +0100

    Added GP3

--- GP3/ex2_2.m
+++ GP3/ex2_2.m
@@ -1,20 +1,20 @@
 tablesize = 1250;
-array = arrayfun(@(i) hashstring(hashfunction(), tablesize), 1:1000);
+array = arrayfun(@(i) hashstring(hashfunctionv2(), tablesize), 1:1000);
 N = zeros(1, tablesize);
+X = zeros(1, 10);
 
 for i = 0:tablesize-1
     N(i + 1) = sum(array == i);
+    X(N(i + 1) + 1) = X(N(i + 1) + 1) + 1;
 end
 
-subplot(1, 3, 1);
-bar(N);
+subplot(1, 2, 1);
+bar(0:9, X);
 
-subplot(1, 3, 2);
-bar(N / tablesize);
+subplot(1, 2, 2);
+bar(N);
 
 fprintf('Factor de carga: %f\n', 1000 / tablesize);
 
-subplot(1, 3, 3);
-bar(N / 1000);
 fprintf('Media de comprimento: %f\n', mean(N));
 fprintf('Variancia de comprimento: %f\n', var(N));
